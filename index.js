5e9202ecf4e7c9e7e8c75f04f0ab4c9f641dc0c7
Joseph Kim <josephkim570@gmail.com>
2020-03-10 13:48:41 -0400

    Target CSS selectors + HTML selectors

--- index.js
+++ index.js
@@ -2,17 +2,26 @@ const request = require('request-promise');
 const fs = require('fs');
 const cheerio = require('cheerio');
 
+// Note: must use jQuery Inject extension
+
 async function main() {
     const html = await request.get
-    ('https://reactnativetutorial.net/css-selectors/lesson2.html'
+    ('https://reactnativetutorial.net/css-selectors/lesson6.html'
     );
     fs.writeFileSync('./test.html', html);
     const $ = await cheerio.load(html);
     $('h2').each((index, element) => {
         console.log($(element).text());
+
+    // CSS selector
+    $('#red').text();
+
+    //HTML selector
+    $('[data-customer="22293"]').text();
+        
+    
     });
-
-
+    
 }
 
 main();
